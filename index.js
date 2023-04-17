197e6ebbd50eb4db8c1fbbd73fa24c217d2940c5
Lê Hoàng Anh <anhlh@synodus.com>
2023-04-17 11:01:20 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// require("hardhat/config");
+require('@matterlabs/hardhat-zksync-deploy');
+const { deployUtils } = require('./src/deployUtils');
+const { TASK_DEPLOY_ZKSYNC } = require('@matterlabs/hardhat-zksync-deploy/dist/task-names');
+
+module.exports = { deployUtils };
+
+task(TASK_DEPLOY_ZKSYNC, 'Runs the deploy scripts for zkSync network')
+    .setAction(async (_, __, runSuper) => {
+        return await runSuper();
+    });
